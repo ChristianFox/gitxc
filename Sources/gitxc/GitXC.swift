0b4eee00031340e67f5716dc2c3979255fb685e0
Christian Fox <christianfox890@icloud.com>
2021-04-23 13:37:42 +0100

    Fixes typo

--- Sources/gitxc/GitXC.swift
+++ Sources/gitxc/GitXC.swift
@@ -6,7 +6,7 @@ import GitXCLib
 struct GitXC: ParsableCommand {
 
 	static let configuration = CommandConfiguration(
-		abstract: "Atool that performs git merge or pull but additionally checks for and resolves conflicts in an Xcode project's info.plist files, specifically conflicts with the CFBundleVersion value.",
+		abstract: "A tool that performs git merge or pull but additionally checks for and resolves conflicts in an Xcode project's info.plist files, specifically conflicts with the CFBundleVersion value.",
 		subcommands: [Merge.self, Pull.self]
 	)
 }
